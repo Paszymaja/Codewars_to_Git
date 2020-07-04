56f0ac4131f548185129e9c13a0b33275ca9b9f9
paszymaja <sprzepiora@hotmail.co.uk>
2020-07-04 17:40:13 +0200

    better test case

--- tests/test.py
+++ tests/test.py
@@ -1,11 +1,12 @@
 import unittest
+import os
 from Codewars_to_Github import page_connect
 
 
 class MyTestCase(unittest.TestCase):
     def test_selenium_login(self):
-        username = input("username: ")
-        password = input("password: ")
+        username = os.getenv('USERNAME')
+        password = os.getenv('PASSWORD')
 
         result = page_connect(username, password)
         file = open('/temp/page_temp.html')
